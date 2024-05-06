9a80867c173e9a785f4aaf37ca848fb95e715f9e
ammar-aljabri <ww.ammaraljabri@gmail.com>
2024-05-06 17:03:22 +0800

    finalize

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World2!");
+
+
+double a = 2.4;
+
+Console.WriteLine(a);
+
+///////////////////////////////////
+
+
